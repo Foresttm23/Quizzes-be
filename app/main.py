0c3dc86e1af5c0761d2febf2904abcbe243d98e0
Foresttm23 <max.gogulia@gmail.com>
2025-10-22 22:34:09 +0300

    Added Dockerfile, .dockerignore and updated README
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
 
@@ -6,6 +7,21 @@ from .routers.health import router as health_router
 app = FastAPI()
 
 app.include_router(health_router)
+
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://other.com"
+    "https://other.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
