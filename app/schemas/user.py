565a95998b0359ac84e20e83b6a63cf81f3dffc6
Foresttm23 <max.gogulia@gmail.com>
2025-10-27 23:29:32 +0200

    Update user schemas in schemas/user.py for more security and validation.

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field, SecretStr
 
 
+# Internal use
 class User(BaseModel):
     id: int
     email: EmailStr
     username: str
+    hashed_password: SecretStr
     created_at: datetime
 
     class Config:
@@ -16,25 +18,27 @@ class User(BaseModel):
 
 class SignUpRequest(BaseModel):
     email: EmailStr
-    username: str
-    password: str
+    username: str = Field(min_length=3, max_length=100)
+    password: SecretStr = Field(min_length=8)
 
 
 class SignInRequest(BaseModel):
-    email: Optional[EmailStr] = None
-    username: Optional[str] = None
-    password: str
+    email: Optional[EmailStr]
+    password: SecretStr
 
 
 class UserUpdateRequest(BaseModel):
     email: Optional[EmailStr] = None
-    username: Optional[str] = None
-    password: Optional[str] = None
-
-
-class UsersListResponse(BaseModel):
-    users: List[User]
+    username: Optional[str] = Field(None, min_length=3, max_length=100)
+    password: Optional[SecretStr] = Field(None, min_length=8)
 
 
 class UserDetailsResponse(BaseModel):
-    user: User
+    id: int
+    email: EmailStr
+    username: str
+    created_at: datetime
+
+
+class UsersListResponse(BaseModel):
+    users: List[UserDetailsResponse]
