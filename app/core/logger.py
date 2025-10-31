790b8b9776a99aafacfb9152e5e8b0f912c580ce
Foresttm23 <max.gogulia@gmail.com>
2025-10-31 21:02:02 +0200

    Setup logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,11 +1,14 @@
-import logging
+from loguru import logger
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[
-        logging.FileHandler("app.log"),
-        logging.StreamHandler(),
-    ]
+# Can be used to delete the provided config by default
+# logger.remove(0)
+
+# Can be added more, like rotation and compression
+logger.add(
+    "logs/app.log",
+    level="DEBUG",
+    encoding="utf-8",
+    enqueue=True,  # For async
+    backtrace=True,  # For detailed errors
+    diagnose=True,  # For detailed errors
 )
--- app/main.py
+++ app/main.py
@@ -1,38 +1,37 @@
-import logging
 from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import app.core.logger
+import app.core.security
 from app.core.config import settings
-from app.db.postgres import sessionmanager
+from app.core.logger import logger
+from app.db.postgres import init_db
 from app.db.redis import redis_client, pool
 from app.routers.health import router as health_router
-
-logger = logging.getLogger(__name__)
+from app.routers.user import router as user_router
 
 
 # From guide https://medium.com/@tclaitken/setting-up-a-fastapi-app-with-async-sqlalchemy-2-0-pydantic-v2-e6c540be4308
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
-    logger.debug(f"Startup")
+    logger.info(f"Startup")
+    sessionmanager = init_db(settings.DB.DATABASE_URL)
 
     yield
     # Shutdown
-    logger.debug(f"Shutdown")
-    ## Close Redis pool and conn
+    logger.info(f"Shutdown")
     await redis_client.close()
     await pool.disconnect()
-    ## Close the DB connection
     await sessionmanager.close()
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(health_router)
+app.include_router(user_router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -43,4 +42,5 @@ app.add_middleware(
 )
 
 if __name__ == '__main__':
-    uvicorn.run("app.main:app", host=settings.APP.HOST, port=settings.APP.PORT, reload=settings.APP.RELOAD)
+    uvicorn.run("app.main:app", host=settings.APP.HOST, port=settings.APP.PORT,
+                reload=settings.APP.RELOAD)
