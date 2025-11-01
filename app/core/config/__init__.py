22cc528abe8dc61a57a4a972a42fab8f49815814
Foresttm23 <max.gogulia@gmail.com>
2025-11-01 11:44:18 +0200

    Update a docker compose to also launch a test db.

--- app/core/config/__init__.py
+++ app/core/config/__init__.py
@@ -2,12 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .app_config import AppSettings
 from .db_config import DBSettings
+from .db_test_config import TestDBSettings
 from .redis_config import RedisSettings
 
 
 class Settings(BaseSettings):
     APP: AppSettings = AppSettings()
     DB: DBSettings = DBSettings()
+    TESTDB: TestDBSettings = TestDBSettings()
     REDIS: RedisSettings = RedisSettings()
 
     model_config = SettingsConfigDict(env_file=".env", extra='ignore')
