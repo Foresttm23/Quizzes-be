94322aa99c9c576f43de099872956ab9ff8c9f61
Foresttm23 <max.gogulia@gmail.com>
2025-11-10 01:03:42 +0200

    Refactor code.
    Add _verify_and_update_password in user_service.py

--- app/db/repository/user_repository.py
+++ app/db/repository/user_repository.py
@@ -1,31 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.exceptions import PasswordReuseException
 from app.db.models.user_model import User as UserModel
-from app.schemas.user_schemas.user_request_schema import UserPasswordUpdateRequest
-from app.utils.password_utils import hash_password, verify_password
 from .base_repository import BaseRepository
 
 
 class UserRepository(BaseRepository[UserModel]):
     def __init__(self, db: AsyncSession):
         super().__init__(model=UserModel, db=db)
-
-    @staticmethod
-    def apply_password_updates(instance: UserModel, new_password_info: UserPasswordUpdateRequest) -> bool:
-        """
-        Helper function for updating password and keeping track of changes.
-        Takes user and new_password_info.
-        """
-        current_password = new_password_info.current_password.get_secret_value()
-        new_password = new_password_info.new_password.get_secret_value()
-
-        # As long as current and new are the same, we raise exception.
-        if current_password == new_password:
-            raise PasswordReuseException()
-
-        if not verify_password(current_password, instance.hashed_password):
-            return False
-
-        instance.hashed_password = hash_password(new_password)
-        return True
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -3,12 +3,16 @@ from typing import Any
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.exceptions import InvalidPasswordException
+from app.core.exceptions import PasswordReuseException
 from app.core.logger import logger
 from app.db.models.user_model import User as UserModel
 from app.db.repository.user_repository import UserRepository
-from app.schemas.user_schemas.user_request_schema import SignUpRequest, UserInfoUpdateRequest, UserPasswordUpdateRequest
+from app.schemas.user_schemas.user_request_schema import SignUpRequest, UserInfoUpdateRequest
+from app.schemas.user_schemas.user_request_schema import UserPasswordUpdateRequest
+from app.schemas.user_schemas.user_response_schema import UserDetailsResponse
 from app.services.base_service import BaseService
-from app.utils.password_utils import hash_password
+from app.utils.password_utils import hash_password, verify_password
 
 
 class UserService(BaseService[UserRepository]):
@@ -23,21 +27,26 @@ class UserService(BaseService[UserRepository]):
         user = await super()._fetch_instance(field_name=field_name, field_value=field_value)
         return user
 
-    async def fetch_users_data_paginated(self, page: int, page_size: int) -> dict[..., list[UserModel]]:
+    async def fetch_users_data_paginated(self, page: int, page_size: int) -> dict[Any, list[UserDetailsResponse]]:
+        # We can now add filter fields.
         users_data = await super()._fetch_instances_data_paginated(page=page, page_size=page_size)
         return users_data
 
     async def create_user(self, user_info: SignUpRequest) -> UserModel:
         """Method for creating a user"""
+        # Since SecretStr(password) will transform to "***" with model_dump(),
+        # we extract password before the call.
         plain_password = user_info.password.get_secret_value()
         hashed_password = hash_password(plain_password)
 
+        user_data = user_info.model_dump()
+        user_data.pop("password")
+
         # Since we don't want to commit real password from the user_info fields
         # We specify directly what fields we need
-        user = UserModel(email=user_info.email, username=user_info.username, hashed_password=hashed_password)
+        user = UserModel(**user_data, hashed_password=hashed_password)
 
         await self.repo.save_changes_and_refresh(instance=user)
-
         logger.info(f"Created new User: {user.id} auth_provider: {user.auth_provider}")
 
         return user
@@ -64,10 +73,7 @@ class UserService(BaseService[UserRepository]):
     async def update_user_password(self, user_id: uuid.UUID, new_password_info: UserPasswordUpdateRequest) -> UserModel:
         """Method for updating user password by id"""
         user = await self.repo.get_instance_by_field_or_404(field_name="id", field_value=user_id)
-        password_changed = self.repo.apply_password_updates(instance=user, new_password_info=new_password_info)
-
-        if not password_changed:
-            return user
+        self._verify_and_update_password(user=user, new_password_info=new_password_info)
 
         await self.repo.save_changes_and_refresh(instance=user)
 
@@ -75,6 +81,19 @@ class UserService(BaseService[UserRepository]):
         logger.debug(f"{self.display_name}: {user.id} changed password")
 
         return user
+
+    @staticmethod
+    def _verify_and_update_password(user: UserModel, new_password_info: UserPasswordUpdateRequest) -> None:
+        current_password = new_password_info.current_password.get_secret_value()
+        new_password = new_password_info.new_password.get_secret_value()
+
+        if current_password == new_password:
+            raise PasswordReuseException()
+
+        if not verify_password(current_password, user.hashed_password):
+            raise InvalidPasswordException()
+
+        user.hashed_password = hash_password(new_password)
 
     async def delete_user_by_id(self, user_id: uuid.UUID) -> None:
         await super()._delete_instance_by_id(instance_id=user_id)
