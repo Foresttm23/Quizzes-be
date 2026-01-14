bd21dbd9c57a164c119a40dba46a54b5d3cefca2
Foresttm23 <max.gogulia@gmail.com>
2026-01-14 20:32:27 +0200

    Minor code cleanup

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -6,7 +6,6 @@ from httpx import AsyncClient
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .caching.utils import CacheManager
 from .config import settings
 from .database import db_session_manager
 from .http_client import http_client_manager
@@ -44,15 +43,8 @@ async def get_redis_client() -> AsyncGenerator[Redis, None]:
 RedisDep = Annotated[Redis, Depends(get_redis_client)]
 
 
-async def get_cache_manager(redis: RedisDep) -> CacheManager:
-    return CacheManager(redis=redis)
-
-
-CacheManagerDep = Annotated[CacheManager, Depends(get_cache_manager)]
-
-
 async def get_http_client() -> AsyncGenerator[AsyncClient, None]:
     return http_client_manager.client()
 
 
-HTTPClientDep = Annotated[AsyncClient, Depends(get_cache_manager)]
+HTTPClientDep = Annotated[AsyncClient, Depends(get_http_client)]
